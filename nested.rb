003d0612505ecf0a527ee3c10903981bd3827333
Jake Mills <jake.mills.05@gmail.com>
2020-07-03 02:57:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+		{ :yukihiro_matsumoto => {
+		  :known_for => "Ruby",
+		  :languages => ["LISP", "C"]
+		} }.merge(programmer_hash)
 end
 
 def changing_alan
